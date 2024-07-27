0a7c03945472c60d9f37362e7e01c6383e09a34b
Fernando Romero <fromerocontato2@gmail.com>
2024-07-27 20:58:22 -0300

    change server url

--- frontend/js/sript.js
+++ frontend/js/sript.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = 'none'
     chat.style.display = 'flex'
 
-    websocket = new WebSocket('ws://localhost:8080')
+    websocket = new WebSocket('wss://chat-backend-i6ww.onrender.com')
     websocket.onmessage = processMessage
     /* websocket.onopen = () => websocket.send(`Usuário: ${user.name} entrou no chat!`) */
 
